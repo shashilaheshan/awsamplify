cc66dc8d1444ba2ac74b8bb253d46cba8eda11a9
shashila heshan <heshanp@dtech.lk>
2019-06-27 11:13:03 +0530

    config_added

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_yXTONhM48', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6drpoe5ddp4moki14o4e9a8ef1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
